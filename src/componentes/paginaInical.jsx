671a14eebe7d2857a44a255a0818c901db63534f
Lucas Sodre <lucasmeinen@yahoo.com.br>
2021-10-25 17:18:59 -0300

    feat:req03

---
+++ src/componentes/paginaInical.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import CartButton from './CartButton';
+
+class paginaInicial extends React.Component {
+  render() {
+    return (
+      <div>
+        <label htmlFor="campoDePesquisa">
+          <input type="text" name="campoDePesquisa" />
+        </label>
+        <h3 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h3>
+        <CartButton/>
+      </div>
+    );
+  }
+}
+
+export default paginaInicial;
