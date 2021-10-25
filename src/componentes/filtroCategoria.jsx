04a4eff227ee4263d452457f9eda11532527d74a
Alex <alex_cubas1.0@hotmail.com>
2021-10-25 19:35:13 -0300

    feat: requisito06

--- src/componentes/filtroCategoria.jsx
+++ src/componentes/filtroCategoria.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { getCategories } from '../services/api';
 
 export default class FiltroCategoria extends Component {
   constructor(props) {
     super(props);
+
+    this.quandoMudar = this.quandoMudar.bind(this);
+
     this.state = {
       categorias: [],
     };
@@ -13,14 +17,27 @@ export default class FiltroCategoria extends Component {
     getCategories().then((data) => this.setState({ categorias: data }));
   }
 
+  quandoMudar({ target }) {
+    const { mudandoCategoria } = this.props;
+    mudandoCategoria(target.id);
+  }
+
   render() {
     const { categorias } = this.state;
     return (
       <div>
         { categorias.map((categoria) => (
           <div key={ categoria.id }>
-            <label data-testid="category" htmlFor={ categoria.id }>
-              <input type="radio" name="categoria" id={ categoria.id } />
+            <label
+              data-testid="category"
+              htmlFor={ categoria.id }
+            >
+              <input
+                type="radio"
+                name="categoria"
+                id={ categoria.id }
+                onChange={ this.quandoMudar }
+              />
               { categoria.name }
             </label>
           </div>
@@ -29,3 +46,7 @@ export default class FiltroCategoria extends Component {
     );
   }
 }
+
+FiltroCategoria.propTypes = {
+  mudandoCategoria: PropTypes.func.isRequired,
+};
