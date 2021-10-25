1a6e45ea5db4007b3f9b7ece6dec28ff85e6d396
douglas oliveira <douglasgama0@gmail.com>
2021-10-25 15:35:33 -0300

    criando pagina para listagem de produtos, requisito 02

---
+++ src/componentes/paginaInical.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class paginaInicial extends React.Component {
+  render() {
+    return (
+      <div>
+        <label>
+          <input type="text" />
+        </label>
+        <h3 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h3>
+      </div>
+    )
+  }
+}
+
+export default paginaInicial;
