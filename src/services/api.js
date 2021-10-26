69d57e734a15b09b4723040a8f65b9725bd69fdd
douglas oliveira <douglasgama0@gmail.com>
2021-10-26 15:18:16 -0300

    fix: req07

--- src/services/api.js
+++ src/services/api.js
@@ -7,6 +7,18 @@ export async function getCategories() {
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  if(!categoryId) {
+    const resultado = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${query}`)
+    .then((response) => response.json());
+  return resultado;
+  }
+
+  if(!query) {
+    const resultado = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`)
+    .then((response) => response.json());
+  return resultado;
+  }
+
   const resultado = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
     .then((response) => response.json());
   return resultado;
